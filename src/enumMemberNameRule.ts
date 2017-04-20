6c6368c3e6d704a36f3583fee1bd59a39ed6834a
Akim McMath <akim.elijah.mcmath@gmail.com>
2017-04-20 07:53:27 -0700

    Refactor: remove enumMemberNameSchema.ts

--- src/enumMemberNameRule.ts
+++ src/enumMemberNameRule.ts
@@ -1,8 +1,37 @@
 import { EnumDeclaration, EnumMember, Node, SourceFile, SyntaxKind, forEachChild } from "typescript";
 import { AbstractWalker, Rules, RuleFailure } from "tslint";
-import { ENUM_MEMBER_NAME_OPTIONS } from "./enumMemberNameSchema";
+
+export interface CaseOption {
+  option: string;
+  description: string;
+  test: (memberName: string) => boolean;
+}
 
 export class Rule extends Rules.AbstractRule {
+
+  public static readonly DEFAULT_CASE_OPTION: CaseOption = {
+    option: "pascal-case",
+    description: "PascalCase",
+    test: (name: string) => /^[A-Z][0-9A-Za-z]*$/.test(name) && (name.length === 1 || /[a-z]/.test(name))
+  };
+
+  public static readonly CASE_OPTIONS: CaseOption[] = [
+    {
+      option: "camel-case",
+      description: "camelCase",
+      test: (name: string) => /^[a-z][0-9A-Za-z]*$/.test(name)
+    },
+    {
+      option: "caps-case",
+      description: "CAPS_CASE",
+      test: (name: string) => /^[A-Z][0-9A-Z_]*[0-9A-Z]$|^[A-Z]$/.test(name)
+    },
+    {
+      option: "snake-case",
+      description: "snake_case",
+      test: (name: string) => /^[a-z][0-9a-z_]*[0-9a-z]$|^[a-z]$/.test(name)
+    }
+  ];
 
   public apply(sourceFile: SourceFile): RuleFailure[] {
     return this.applyWithWalker(new EnumMemberNameWalker(sourceFile, this.ruleName, this.ruleArguments));
@@ -38,15 +67,15 @@ class EnumMemberNameWalker extends AbstractWalker<Set<string>> {
   private checkEnumMemberName(node: EnumMember): void {
     const name = node.name.getText();
 
-    for (const { option, description, validate } of ENUM_MEMBER_NAME_OPTIONS) {
+    for (const { option, description, test } of Rule.CASE_OPTIONS) {
       if (this.options.has(option)) {
-        if (!validate(name)) this.registerFailure(node, description);
+        if (!test(name)) this.registerFailure(node, description);
         return;
       }
     }
 
-    if (!ENUM_MEMBER_NAME_OPTIONS[0].validate(name)) {
-      this.registerFailure(node, ENUM_MEMBER_NAME_OPTIONS[0].description);
+    if (!Rule.DEFAULT_CASE_OPTION.test(name)) {
+      this.registerFailure(node, Rule.DEFAULT_CASE_OPTION.description);
     }
   }
 
